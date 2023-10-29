b694fc10c52b061616f3ee4edd825b0840576993
ibrahim-sert <evo3506@gmail.com>
2023-10-29 22:09:43 +0300

    task-nextjs edited

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,254 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// "use client"; // don't forget this part if you use app dir to mark the whole
+// file as client-side components
+import { Card, Grid, Typography } from "@mui/material";
+import axios from "axios";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import dynamic from "next/dynamic";
+const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import * as Yup from "yup";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const validationSchema = Yup.object().shape({
+  date: Yup.string().required("Ay seçmelisiniz"),
+  fValue: Yup.number()
+    .typeError("Lütfen bir numara girin")
+    .required("Numara alanı gereklidir"),
+  bValue: Yup.number()
+    .typeError("Lütfen bir numara girin")
+    .required("Numara alanı gereklidir"),
+});
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  // const [sonuc, setSonuc] = useState("");
+  const BASE_URL = "http://localhost:4000/backend";
+
+  const getData = async () => {
+    try {
+      const { data } = await axios(BASE_URL);
+      setPosts(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postData = async (info) => {
+    console.log("post");
+    try {
+      await axios.post(`${BASE_URL}`, info);
+      await getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const putData = async (info) => {
+    const { date } = info;
+    const putValue = posts.find((p) => p.date === date);
+    try {
+      await axios.put(`${BASE_URL}/${putValue.id}`, info);
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const delData = async (info) => {
+    const { date } = info;
+    const putValue = posts.find((p) => p.date === date);
+    try {
+      await axios.delete(`${BASE_URL}/${putValue.id}`);
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(posts);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const option = {
+    chart: {
+      id: "apexchart-example",
+    },
+    xaxis: {
+      categories: posts?.map((d) => d.date) || [],
+    },
+  };
+
+  //  posts?.[0]?.data
+  //   ?.sort((a, b) => a.date - b.date)
+  //   .map((d) => d.date),
+
+  const series = [
+    {
+      name: "Frontend Salaries",
+      data: Object.values(posts)?.map((v) => v.fValue),
+      // posts?.[0]?.data
+      //   ?.sort((a, b) => a.date - b.date)
+      //   .map((d) => d.value),
+    },
+    {
+      name: "Backend Salaries",
+      data: Object.values(posts)?.map((v) => v.bValue),
+      // posts?.[1]?.data
+      //   ?.sort((a, b) => a.date - b.date)
+      //   .map((d) => d.value),
+    },
+  ];
+  console.log(option);
+  console.log(series);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <Formik
+      initialValues={{
+        id: 0,
+        date: 0,
+        fValue: 0,
+        bValue: 0,
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        // e.preventDefault();
+        if (values?.id) {
+          putData(values);
+        } else {
+          postData(values);
+        }
+
+        actions.resetForm();
+        actions.setSubmitting(false);
+      }}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {(formikProps) => (
+        <Grid
+          container
+          justifyContent="center"
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gap={2}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Grid item gridColumn="span 8" height={"700px"}>
+            <Card>
+              <Typography>Frontend Salaries</Typography>
+              <ApexChart
+                type="line"
+                options={option}
+                series={series}
+                height={500}
+                width={500}
+              />
+            </Card>
+          </Grid>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <Grid item gridColumn="span 4">
+            <Form
+              className="basis-1/3 h-[395px] rounded-lg bg-[#111827] flex flex-col justify-center "
+              onSubmit={formikProps.handleSubmit}
+            >
+              <div className="mb-2 mt-6 flex  flex-col justify-center items-center h-full mx-auto w-80">
+                <div className="flex flex-col gap-2 w-full text-white">
+                  <label htmlFor="date">Date:</label>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+                  <Field
+                    name="date"
+                    id="fed"
+                    type="number"
+                    variant="outlined"
+                    // value={info?.date}
+                    // onChange={handleChange}
+                    value={formikProps.values.date}
+                    onChange={formikProps.handleChange}
+                    required
+                    className="mb-2 bg-gray-500 text-white placeholder-white placeholder:opacity-50 text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Enter a year"
+                  />
+                  <ErrorMessage
+                    className=" text-red-700 mb-2 invalid-feedback"
+                    name="date"
+                    component="div"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 w-full text-white">
+                  <label htmlFor="fValue">Frontend Dev. Salary:</label>
+                  <Field
+                    name="fValue"
+                    id="fValue"
+                    type="number"
+                    variant="outlined"
+                    // value={info?.frontendSalary}
+                    // onChange={handleChange}
+                    value={formikProps.values.fValue}
+                    onChange={formikProps.handleChange}
+                    required
+                    className="mb-2 bg-gray-500 text-white placeholder-white placeholder:opacity-50 text-sm rounded-lg block w-full p-2.5  "
+                    placeholder="Enter Frontend Dev. Salary"
+                  />
+                  <ErrorMessage
+                    className=" text-red-700 mb-2 invalid-feedback"
+                    name="fValue"
+                    component="div"
+                  />
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <div className="flex flex-col gap-2 mb-5 w-full text-white">
+                  <label htmlFor="bValue">Backend Dev. Salary:</label>
+                  <Field
+                    name="bValue"
+                    id="bValue"
+                    type="number"
+                    variant="outlined"
+                    value={formikProps.values.bValue}
+                    onChange={formikProps.handleChange}
+                    required
+                    className="mb-2 bg-gray-500 text-white placeholder-white placeholder:opacity-50 text-sm rounded-lg block w-full p-2.5 "
+                    placeholder="Enter Backend Dev. Salary"
+                  />
+                  <ErrorMessage
+                    className=" text-red-700 mb-2"
+                    name="bValue"
+                    component="div"
+                  />
+                </div>
+                <div className=" w-full flex flex-row justify-around gap-3">
+                  <button
+                    type="submit"
+                    className="bg-green-700 rounded-xl my-2 py-1 px-4 w-full text-white "
+                  >
+                    Submit
+                  </button>
+                  <button
+                    onClick={formikProps.resetForm}
+                    type="reset"
+                    className="bg-black rounded-xl my-2 py-1 px-4 w-full text-white"
+                  >
+                    Reset
+                  </button>
+                  <button
+                    onClick={() => putData(formikProps.values)}
+                    type="button"
+                    className="bg-red-700 rounded-xl my-2 py-1 px-4 w-full text-white"
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => delData(formikProps.values)}
+                    type="button"
+                    className="bg-red-700 rounded-xl my-2 py-1 px-4 w-full text-white"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </Form>
+          </Grid>
+        </Grid>
+      )}
+    </Formik>
+  );
 }
